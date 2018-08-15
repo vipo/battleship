d8796a4bdb459c96cad36c8f85ed92178ddccf6d
Vi Po <pi@homedir.eu>
2018-08-15 17:58:50 +0300

    reuse terms of ct

--- app/Main.hs
+++ app/Main.hs
@@ -18,11 +18,12 @@ import Control.Monad.IO.Class
 
 import qualified Data.Aeson as A
 import qualified Data.BEncode as Ben
+import qualified Data.ByteString as BS
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as T
 import GHC.Generics
 import GHC.TypeLits
-import Network.HTTP.Media((//))
+import Network.HTTP.Media((//), MediaType)
 import Network.Wai.Handler.Warp
 import Servant
 import Servant.API.ContentTypes (Accept)
@@ -36,33 +37,42 @@ instance FromHttpApiData I.GameVariation where
   parseUrlPiece "t-shapes" = Right I.TShape
   parseUrlPiece a = Left $ T.concat ["Unknown game: ", showt a]
 
+ct :: BS.ByteString
+ct = "application"
+
+bencMt :: BS.ByteString -> MediaType
+bencMt suff = ct // BS.concat "bencoding" suff
+
+jsonMt :: BS.ByteString -> MediaType
+jsonMt suff = ct // BS.concat "json" suff
+
 data JSONNoLists
 instance Accept JSONNoLists where
-  contentType _ = "application" // "json+nolists"
+  contentType _ = jsonMt "+nolists"
 instance A.ToJSON a => MimeRender JSONNoLists a where
   mimeRender _ = A.encode . J.withOutLists . A.toJSON
   
 data JSONNoMaps
 instance Accept JSONNoMaps where
-  contentType _ = "application" // "json+nomaps"
+  contentType _ = jsonMt "+nomaps"
 instance A.ToJSON a => MimeRender JSONNoMaps a where
   mimeRender _ = A.encode . J.withOutMaps . A.toJSON
 
 data Bencoding
 instance Accept Bencoding where
-  contentType _ = "application" // "bencoding"
+  contentType _ = bencMt ""
 instance Ben.BEncode a => MimeRender Bencoding a where
   mimeRender _ = Ben.encode . Ben.toBEncode
 
 data BencodingNoLists
 instance Accept BencodingNoLists where
-  contentType _ = "application" // "bencoding+nolists"
+  contentType _ = bencMt "+nolists"
 instance Ben.BEncode a => MimeRender BencodingNoLists a where
   mimeRender _ = Ben.encode . B.withOutLists . Ben.toBEncode
 
 data BencodingNoMaps
 instance Accept BencodingNoMaps where
-  contentType _ = "application" // "bencoding+nomaps"
+  contentType _ = bencMt "+nomaps"
 instance Ben.BEncode a => MimeRender BencodingNoMaps a where
   mimeRender _ = Ben.encode . B.withOutMaps . Ben.toBEncode
 
